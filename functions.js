dca57b9404a2f997dcbf7167b96e5c2fe0edd2f6
vinnyvilasboa <vinnycesca@gmail.com>
2021-08-09 12:37:23 -0400

    promises and functions 1

---
+++ functions.js
@@ -0,0 +1,11 @@
+const greeting = function(taco) {
+    // anything inside of here will execute when called
+    console.log("Good morning", taco);
+}
+
+let myName = "Josh"
+let name2 = "Brian"
+greeting(myName);
+greeting(name2);
+
+
